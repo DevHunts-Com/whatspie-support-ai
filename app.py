568da890a96ace143207f892daed964cad3d1561
Tarikh Agustia <agustia.tarikh150@gmail.com>
2024-06-05 07:12:58 +0700

    fix : double execution

--- app.py
+++ app.py
@@ -110,3 +110,8 @@ def handle_hooks(item: WhatspieBody):
         "body": response['answer'],
         "simulation": "true",
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
